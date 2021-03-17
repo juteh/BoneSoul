2455237dd932e7b579e7882b7ac3a4b7081eb2be
Leon <leon.adam.91@gmail.com>
2021-03-17 08:25:50 +0100

    add packages for tilemaps ans sprites

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    [SerializeField] float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 newVelocity = target.GetComponent<Rigidbody2D>().velocity;
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref newVelocity, smoothSpeed * Time.deltaTime);
+        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+        // funny effect :D
+        // transform.LookAt(target);
+    }
+}
